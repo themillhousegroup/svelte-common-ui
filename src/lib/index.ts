ca25fd087c161c2c8da346271523c297a90b6552
themillhousegroup <john.marshall@ovoenergy.com.au>
2025-07-02 12:34:23 +1000

    Attempt export from theming - types etc

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,11 +2,36 @@
 import ModalMatte from './ModalMatte.svelte';
 import RadioControl from './RadioControl.svelte';
 import SliderControl from './SliderControl.svelte';
+import ThemeProvider from './ThemeProvider.svelte';
+import type { 
+  CSSThemedPropertyName,
+  ThemedPropertyNameList,
+  CoreThemePropertyName,
+  CorePropertyNameList,
+  AllThemePropertyNames,
+  AllThemePropertyName,
+  Theme,
+  ThemeName
+ } from './theming';
+
+import {
+  CORE_THEME_PROPERTY_NAMES,
+  CORE_THEME_NAMES
+} from './themeing';
+
 export {
   ModalMatte,
   RadioControl,
-  SliderControl
+  SliderControl,
+  ThemeProvider,
+  type CSSThemedPropertyName,
+  type ThemedPropertyNameList,
+  type CoreThemePropertyName,
+  type CorePropertyNameList,
+  type AllThemePropertyNames,
+  type AllThemePropertyName,
+  type Theme,
+  type ThemeName,
+  CORE_THEME_PROPERTY_NAMES,
+  CORE_THEME_NAMES
 };
-
-// export * from './ThemeProvider.svelte';
-// export * from './theming';
